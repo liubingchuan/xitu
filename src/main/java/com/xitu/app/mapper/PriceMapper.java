ff15f61b1c39c0524ecb129f9c1a6249b9036826
user.email <bingchuan.lbc@alibaba-inc.com>
2019-04-27 17:32:48 +0800

    fix

--- src/main/java/com/xitu/app/mapper/PriceMapper.java
+++ src/main/java/com/xitu/app/mapper/PriceMapper.java
@@ -1,6 +1,7 @@
 package com.xitu.app.mapper;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.InsertProvider;
 import org.apache.ibatis.annotations.Mapper;
@@ -26,6 +27,12 @@ public interface PriceMapper {
 	
 	@Select("SELECT * FROM xitu_price where update_time = #{updateTime}")
 	List<Price> getPricesByUpdateTime(@Param("updateTime") String updateTime);
+	
+	@Select("SELECT name FROM xitu_price group by name")
+	List<String> getPricesGroupByName();
+	
+	@Select("SELECT avg(avg) FROM xitu_price where update_time >= #{start} and update_time < #{end} and name = #{name} group by name")
+	String getAvgPricesGroupByName(@Param("start") String start, @Param("end") String end, @Param("name") String name);
 	
 	@Select("SELECT MAX(update_time) FROM xitu_price;")
 	String getLatestUpdateTime();
